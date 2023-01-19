caff765e373af9a4e58870899177e7c2811f49ac
lepffm.kim <lepffm@gmail.com>
2023-01-19 23:28:15 +0900

    [FEATURE] add kotlin maven project (#1)
    # maven kotlin project
    - change junit4 to 5
    - downgrade kotlin 1.8 to 1.6 ( codespace limited)
    - set ignore target directory to .gitignore
    - add mockk library
    - add launch json setting

---
+++ kotlin-maven-sample/src/main/kotlin/com/pffm/kotlin/samples/Hello.kt
@@ -0,0 +1,8 @@
+package com.pffm.kotlin.samples
+
+class Hello{
+    fun main(args: Array<String>) {
+        println("Hello, World ??")
+    }
+}
+
---
+++ kotlin-maven-sample/src/test/kotlin/com/pffm/kotlin/samples/HelloTest.kt
@@ -0,0 +1,11 @@
+package com.pffm.kotlin.samples
+
+import kotlin.test.assertEquals
+import org.junit.jupiter.api.Test;
+
+class HelloTest {
+    @Test
+    fun `when main, then print`(){
+        Hello().main(emptyArray())
+    }
+}
